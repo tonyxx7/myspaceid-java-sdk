f7d9ed8a1c8687687fa1216faad1aa392d5c0a5f
kiamchoo <kiamchoo@2e2e4d80-0e6f-11de-a42d-873b86d837ff>
2009-03-12 00:34:23 +0000

    New

---
+++ sample/hybrid/hybrid-login.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Login -- MySpaceID OpenID-OAuth Hybrid Sample</title>
+	<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8" />
+</head>
+<body>
+<img src='../img/mdp_logo.jpg'/><br>
+<h2>MySpaceID OpenID-OAuth Hybrid Sample</h2>
+<form action="consumer">
+  <img src="/myspaceid-sample/img/openid.jpg"/> <br/>
+  Enter your OpenID login: <input type="text" name="openid_identifier" size="60"/> <input type="submit" value="Login"/>
+</form>
+</body>
+</html>
---
+++ src/com/myspace/myspaceid/oauth/OAuthToken.java
@@ -0,0 +1,43 @@
+package com.myspace.myspaceid.oauth;
+
+/**
+ * Class that encapsulates an OAuth token.
+ */
+public class OAuthToken 
+{
+	protected String key;
+	protected String secret;
+
+	public OAuthToken(String key, String secret) {
+		this.key = key;
+		this.secret = secret;
+	}
+
+	public OAuthToken(String str) {
+		int i = str.indexOf("=");
+		int j = str.indexOf("&", i + 1);
+		key = str.substring(i + 1, j).trim();
+		int k = str.indexOf("=", j + 1);
+		secret = str.substring(k + 1, str.length()).trim();
+	}
+
+	public String getSecret() {
+		return secret;
+	}
+
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key  = key;
+	}
+
+	public String toString() {
+		return "OAuthToken: key = " + key + " , secret = " + secret;
+	}
+}
